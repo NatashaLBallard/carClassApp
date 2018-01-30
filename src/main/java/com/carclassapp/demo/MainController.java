962062f81d9527cda4c8c50161e5faaa4596b09e
NatashaLBallard <nbitcareer@gmail.com>
2018-01-30 10:34:31 -0500

    constructing basic set up

--- src/main/java/com/carclassapp/demo/MainController.java
+++ src/main/java/com/carclassapp/demo/MainController.java
@@ -2,15 +2,30 @@ package com.carclassapp.demo;
 
 public class MainController {
 
+   public class Vehicle{
+
+
+
     public class Car {
+        private String make;
+        private String color;
+        private int speed;
+
+        public void main (String[] args) {
+            Car myCar = new Car("Jeep", "green", 65);
+            make = "Jeep";
+            color = "green";
+            speed = 65;
+
+            
+        }
 
 
 
 
-        
     }
 
-
+}
 
 
 
